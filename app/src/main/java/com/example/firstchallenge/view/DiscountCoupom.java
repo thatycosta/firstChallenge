00b402f731a8ceeac40ab1143277f6d14ad86ba4
Tauane Costa <tauanedasilvacosta@gmail.com>
2021-06-20 16:38:50 -0300

    Added Toast Message to Discount Coupom

--- app/src/main/java/com/example/firstchallenge/view/DiscountCoupom.java
+++ app/src/main/java/com/example/firstchallenge/view/DiscountCoupom.java
@@ -1,7 +1,10 @@
 package com.example.firstchallenge.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +15,10 @@ public class DiscountCoupom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discount_coupom);
+    }
 
+    public void redeemCoupom(View view){
+        EditText email = findViewById(R.id.editTextEmailAddress);
+        Toast.makeText(this, "Cupom enviado para " + email.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
